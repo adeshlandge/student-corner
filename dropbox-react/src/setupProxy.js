8e73e2a72b03653463e9d30775d30d32faabaf6b
Adesh Landge <37284267+adeshlandge@users.noreply.github.com>
2022-11-24 17:15:04 -0800

    Delete dropbox-react directory

--- dropbox-react/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware"); //Adding proxy to add domain to the relative links
-module.exports = function (app) {
-  app.use(
-    "/registerUser",
-    createProxyMiddleware({
-      target: "http://localhost:8080",
-    })
-  );
-};
